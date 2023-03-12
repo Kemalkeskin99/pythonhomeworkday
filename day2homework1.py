01c2e42de0841c5cd68b2cc8fb78ca7baa6d7102
Kemalkeskin99 <dkdk999k@gmail.com>
2023-03-12 17:35:26 +0300

    update

--- day2homework1.py
+++ day2homework1.py
@@ -1,25 +1,36 @@
 
 students=["Nazli Güneş"]
 
+#question1;
 students.append("Kemal Keskin")
 #print(students)
 
+#question2;
 students.pop(0)
 #print(students)
 
+#question3;
 students.extend(["Umut Aksu","Ayşe Yılmaz"])
 #print(students)
 
+#question4;
 for i in range(len(students)):
     print(students[i])
 
+#question5;
+#path1
+print("öğrenci Numarası: ",students.index("Kemal Keskin")," || ","Öğrencinin adı:",students[0])
+print("öğrenci Numarası: ",students.index("Umut Aksu")," || ","Öğrencinin adı:",students[1])
+print("öğrenci Numarası: ",students.index("Ayşe Yılmaz")," || ","Öğrencinin adı:",students[2])
+
+#path2
 for i in range(len(students)):
     print("Öğrenci ad soyad :",students[i],"||","Öğrenci numarası :",i)
 
 
+#question6;
 i=0
 while(i<len(students)):
     students.pop()
     i+=1
     print(students)
-    
